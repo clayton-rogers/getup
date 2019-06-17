67e10419b45a4a12d71e42c83b851b232803c98d
Clayton Rogers <claytonrogers53@gmail.com>
2019-06-16 23:43:33 -0400

    Preliminary OSCAL testing

--- OSCALclient/OSCALclient.ino
+++ OSCALclient/OSCALclient.ino
@@ -1,13 +1,21 @@
 /*
-
-
-
+  NOTE:
+    This sketch is running at the built-in 8 Mhz
 */
 
 void setup() {
-	
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void pulseLED() {
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1);
+	digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-	
+	while (true) {
+		delay(10000);
+		pulseLED();
+	}
 }
--- OSCALserver/OSCALserver.ino
+++ OSCALserver/OSCALserver.ino
@@ -4,10 +4,23 @@
 
 */
 
+const int PULSE_PIN = 3;
+
 void setup() {
-	
+	Serial.begin(115200);
+
+	Serial.print("Hello world\n");
+
+	pinMode(PULSE_PIN, INPUT);
 }
 
 void loop() {
+	while (LOW == digitalRead(PULSE_PIN)) {};
+	const unsigned long start_time = micros();
+	delay(500); // delay at least half a second to clear the pulse
 	
+	while (LOW == digitalRead(PULSE_PIN)) {};
+	const unsigned long end_time = micros();
+	Serial.println(end_time - start_time);
+	delay(500); // again delay at least half a second to clear the pulse
 }
